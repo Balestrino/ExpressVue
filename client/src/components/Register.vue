4b6d93cad5d8bcb0c7a7678cdc39f664d643a070
Salvatore Balestrino <salvatore.balestrino@gmail.com>
2019-05-10 14:50:27 +0200

    vue'd the register page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,16 +1,24 @@
 <template>
-    <div>
-        <h1>Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
 
-        <input type="text" name="email" placeholder="email" v-model="email" />
-        <br>
-        <input type="password" name="password" placeholder="password" v-model="password" />
-        <br>
-        <!-- calling register method -->
-        <button @click="register">Register</button>
-        <div class="error" v-html="error" />
-    </div>
-
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="text" name="email" placeholder="email" v-model="email" />
+          <br>
+          <input type="password" name="password" placeholder="password" v-model="password" />
+          <br>
+          <div class="error" v-html="error" />
+          <br>
+          <!-- calling register method -->
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <!-- script is the controller -->
